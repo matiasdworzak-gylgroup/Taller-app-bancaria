2eab0b1e9dbc229ffbcb6389b60e480093e6ae39
Matias Dworzak <medworzak@gylgroup.com>
2026-04-14 16:04:03 -0300

    Sucursales hardcodeadas en el banco con sus respectivos clientes

--- app-bancaria/src/main/java/App.java
+++ app-bancaria/src/main/java/App.java
@@ -1,53 +1,12 @@
 public class App {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         Banco bancoPatagonia = Banco.getInstancia();
-        Admin adminBancoPatagoniaSucursalCaballito = new Admin(bancoPatagonia.getSucursales().getFirst());
+        Admin adminBancoPatagoniaSucursalCaballito = new Admin(bancoPatagonia.getSucursales().get(0));
+        Admin adminBancoPatagoniaSucursalMataderos = new Admin(bancoPatagonia.getSucursales().get(1));
+        Admin adminBancoPatagoniaSucursalParquePatricios = new Admin(bancoPatagonia.getSucursales().get(2));
+
+        
 
 
-
-        //Creacion de usuarios
-        UsuarioCliente user1 = new UsuarioCliente("Jose Marmol", "calle 1", TipoDeCuenta.SUELDO);
-        UsuarioCliente user2 = new UsuarioCliente("Axel Jota", "calle 2", TipoDeCuenta.CORRIENTE);
-        UsuarioCliente user3 = new UsuarioCliente("Sofia Acevedo", "calle 3", TipoDeCuenta.SUELDO);
-        UsuarioCliente user4 = new UsuarioCliente("Tomas Benitez", "calle 4", TipoDeCuenta.AHORRO);
-
-        //Alta usuarios
-        adminBancoPatagoniaSucursalCaballito.altaUsuario(user1);
-        adminBancoPatagoniaSucursalCaballito.altaUsuario(user2);
-        adminBancoPatagoniaSucursalCaballito.altaUsuario(user3);
-        adminBancoPatagoniaSucursalCaballito.altaUsuario(user4);
-
-        //Depositar en sus cuentas
-        adminBancoPatagoniaSucursalCaballito.depositar(user2, 14752.34);
-        adminBancoPatagoniaSucursalCaballito.depositar(user1, 5000.50);
-        adminBancoPatagoniaSucursalCaballito.depositar(user1, 10000.50);
-        adminBancoPatagoniaSucursalCaballito.depositar(user3, 40000.00);
-
-        //Listar usuarios
-        System.out.println("Balance de usuarios activos: ");
-        System.out.println("-----------------------------");
-        adminBancoPatagoniaSucursalCaballito.listarUsuariosConSusBalances();
-
-        //Transacciones
-        adminBancoPatagoniaSucursalCaballito.transferir(user2, user1, 5000.00);
-        adminBancoPatagoniaSucursalCaballito.transferir(user2, user1, 10000.00);
-        adminBancoPatagoniaSucursalCaballito.transferir(user3, user4, 22000.00);
-        adminBancoPatagoniaSucursalCaballito.transferir(user4, user2, 22001.00);
-
-        //Dar de baja a un usuario
-        System.out.println("Dar de baja el usuario de Axel ");
-        adminBancoPatagoniaSucursalCaballito.bajaUsuario(user2);
-        System.out.println("-----------------------------");
-        adminBancoPatagoniaSucursalCaballito.transferir(user2, user3, 1500.00);
-
-        //Volver a revisar el balance y si efectivamente esta dadod de baja el usuario
-        System.out.println("-----------------------------");
-        System.out.println("Balance de usuarios activos: ");
-        System.out.println("-----------------------------");
-        adminBancoPatagoniaSucursalCaballito.listarUsuariosConSusBalances();
-
-        //Historial de transacciones x usuario
-        System.out.println(adminBancoPatagoniaSucursalCaballito.obtenerHistorialDeTransaccionesDelUsuario(user2));
-        System.out.println(adminBancoPatagoniaSucursalCaballito.obtenerHistorialDeTransaccionesDelUsuario(user4));
     }
 }
--- app-bancaria/src/main/java/Banco.java
+++ app-bancaria/src/main/java/Banco.java
@@ -32,6 +32,22 @@ public final class Banco {
         UsuarioCliente user3 = new UsuarioCliente("Sofia Acevedo", "calle 3", TipoDeCuenta.SUELDO);
         UsuarioCliente user4 = new UsuarioCliente("Tomas Benitez", "calle 4", TipoDeCuenta.AHORRO);
         ArrayList<UsuarioCliente> listaUsuariosCaballito = new ArrayList<>(Arrays.asList(user1, user2, user3, user4));
+        sucursales.get(0).setUsuariosActivos(listaUsuariosCaballito);
+
+        UsuarioCliente user5 = new UsuarioCliente("Marcos Sastre", "calle 5", TipoDeCuenta.SUELDO);
+        UsuarioCliente user6 = new UsuarioCliente("Tatiana Rodriguez", "calle 6", TipoDeCuenta.CORRIENTE);
+        UsuarioCliente user7 = new UsuarioCliente("Josue Jose", "calle 7", TipoDeCuenta.AHORRO);
+
+        ArrayList<UsuarioCliente> listaUsuariosMataderos = new ArrayList<>(Arrays.asList(user5, user6, user7));
+        sucursales.get(1).setUsuariosActivos(listaUsuariosMataderos);
+
+        UsuarioCliente user9 = new UsuarioCliente("Matias Dworzak", "calle 8", TipoDeCuenta.AHORRO);
+        UsuarioCliente user10 = new UsuarioCliente("Lucas Bravo", "calle 9", TipoDeCuenta.AHORRO);
+        UsuarioCliente user11 = new UsuarioCliente("Gustavo Narancio", "calle 10", TipoDeCuenta.SUELDO);
+        UsuarioCliente user12 = new UsuarioCliente("Uri Yurquina", "calle 11", TipoDeCuenta.AHORRO);
+        ArrayList<UsuarioCliente> listaUsuariosParquePatricios = new ArrayList<>(Arrays.asList(user9, user10, user11, user12));
+        sucursales.get(2).setUsuariosActivos(listaUsuariosParquePatricios);
+
     }
 
     public ArrayList<Sucursal> getSucursales() {
